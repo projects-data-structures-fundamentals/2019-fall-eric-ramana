93fc223d6dca0abb60b69f448e4c34bb15cf6b6d
Eric O'Neil <ecf59@wildcats.unh.edu>
2019-12-01 20:36:15 -0500

    implemented exercise_by_gender() to gather exercise data of genders

--- analysis/data_analysis.py
+++ analysis/data_analysis.py
@@ -7,18 +7,37 @@ Created: 2019-12-01
 Updated:
 """
 import csv
+import json
 
 class Analysis(object):
     """
     Contains functions for working with survey_results_public.csv
     """
 
-    def exercise_by_gender(self, filename):
+    survey_csv = 'C:\\Users\\ecoun\\OneDrive\\Desktop\\survey_results_public.csv'
+    # survey_csv = 'test_survey.csv'
+
+    def exercise_by_gender(self):
         """
         Finds how often each gender exercises based on survey responses and
             adds it to a dictionary.
-        filename: string that has the name of the CSV file
         Returns: nested dictionary in which the keys are genders and the values
             are a second dictionary with keys of amount of times they exercise
             and values of response numbers
         """
+        with open(Analysis.survey_csv, 'r', encoding='cp1252', errors='ignore') as csvfile:
+            reader = csv.DictReader(csvfile)
+            exercise_dict = {}
+            for row in reader:
+                if row['Gender'] not in exercise_dict:
+                    exercise_dict[row['Gender']] = {row['Exercise']: 1}
+                elif row['Exercise'] not in exercise_dict[row['Gender']]:
+                    exercise_dict[row['Gender']][row['Exercise']] = 1
+                else:
+                    exercise_dict[row['Gender']][row['Exercise']] = exercise_dict[row['Gender']][row['Exercise']] + 1
+        return exercise_dict
+
+
+if __name__ == '__main__':
+    analysis = Analysis()
+    print(json.dumps(analysis.exercise_by_gender(), indent=1))
