3eaebe2ace8eef31853b486b21071d20def1fb55
Eric O'Neil <ecf59@wildcats.unh.edu>
2019-12-14 18:07:41 -0500

    added main.py to run entire program

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+main.py
+Execution of the Stack Overflow Developer Survey Project.
+Eric O'Neil
+Ramana Kondaveeti
+Created: 2019-12-14
+Updated:
+"""
+
+import data_analysis as analyze
+
+def main():
+    print(analyze.Analysis.median_salary_by_gender())
+
+
+if __name__ == '__main__':
+    main()
+    # print(json.dumps(analysis.exercise_by_gender(), indent=1, sort_keys=True))
+    # print(analysis.exercise_by_gender())
+    # print(json.dumps(analysis.median_salary_by_gender(), indent=1, sort_keys=True))
+    # print(json.dumps(analysis.job_satisfaction_by_gender(), indent=1, sort_keys=True))
