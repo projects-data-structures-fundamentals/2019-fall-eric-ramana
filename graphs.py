ca313e4d03e0fbcbb1168967502a52bbe209b10e
Eric O'Neil <ecf59@wildcats.unh.edu>
2019-12-15 14:38:13 -0500

    implemented a second graph creator for salaries successfully

--- graphs.py
+++ graphs.py
@@ -15,11 +15,17 @@ import pandas as pd
 
 def create_graph(type_of_data):
     if type_of_data == 'exercise':
-        exercise_dict = analysis.exercise_by_gender()
-
-    df = pd.DataFrame(exercise_dict)
-
-    ax = df.plot(kind="bar", rot=0, title="Exercise Frequency")
-    for p in ax.patches:
-        ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
-    plt.show()
+        working_dict = analysis.exercise_by_gender()
+        df = pd.DataFrame(working_dict)
+        ax = df.plot(kind="bar", rot=0, title="Exercise Frequency", colormap='tab20')
+        for p in ax.patches:
+            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+        plt.show()
+    elif type_of_data == 'salary':
+        working_dict = analysis.median_salary_by_gender()
+        index = ['Genders']
+        df = pd.DataFrame(working_dict, index=index)
+        ax = df.plot(kind="bar", rot=0, title="Median Salary By Gender", colormap='tab20')
+        for p in ax.patches:
+            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+        plt.show()
