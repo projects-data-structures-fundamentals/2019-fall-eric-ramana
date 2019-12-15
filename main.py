28e3e83c6927c75e53b1d8aaa121c89fc9504c08
Eric O'Neil <ecf59@wildcats.unh.edu>
2019-12-15 15:16:20 -0500

    complied main.py with pycodestyle standards

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ Execution of the Stack Overflow Developer Survey Project.
 Eric O'Neil
 Ramana Kondaveeti
 Created: 2019-12-14
-Updated:
+Updated: 2019-12-15
 """
 
 import menu as menu
 
+
 def main():
     menu.program_run()
-    # print(analyze.Analysis.median_salary_by_gender())
 
 
 if __name__ == '__main__':
     main()
-    # print(json.dumps(analysis.exercise_by_gender(), indent=1, sort_keys=True))
-    # print(analysis.exercise_by_gender())
-    # print(json.dumps(analysis.median_salary_by_gender(), indent=1, sort_keys=True))
-    # print(json.dumps(analysis.job_satisfaction_by_gender(), indent=1, sort_keys=True))
