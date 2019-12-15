2ecd4572e9937ecf48e4de5e17231f66f8a7c1a9
Eric O'Neil <ecf59@wildcats.unh.edu>
2019-12-15 15:16:05 -0500

    complied graphs.py with pycodestyle standards

--- graphs.py
+++ graphs.py
@@ -13,26 +13,47 @@ import numpy as np
 import data_analysis as analysis
 import pandas as pd
 
+
 def create_graph(type_of_data):
+    """
+    Creates a graph of data from the Stack Overflow Developer Survey based on
+        the option selected by the user. Presents the graph on the user's
+        screen.
+    """
     if type_of_data == 'exercise':
         working_dict = analysis.exercise_by_gender()
         df = pd.DataFrame(working_dict)
-        ax = df.plot(kind="bar", rot=0, title="Exercise Frequency By Gender", colormap='tab20')
+        ax = df.plot(kind="bar",
+                     rot=0,
+                     title="Exercise Frequency By Gender",
+                     colormap='tab20')
         for p in ax.patches:
-            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2.,
+                        p.get_height()), ha='center', va='center',
+                        xytext=(0, 10), textcoords='offset points')
         plt.show()
     elif type_of_data == 'salary':
         working_dict = analysis.median_salary_by_gender()
         index = ['Genders']
         df = pd.DataFrame(working_dict, index=index)
-        ax = df.plot(kind="bar", rot=0, title="Median Salary By Gender", colormap='tab20')
+        ax = df.plot(kind="bar",
+                     rot=0,
+                     title="Median Salary By Gender",
+                     colormap='tab20')
         for p in ax.patches:
-            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2.,
+                        p.get_height()), ha='center', va='center',
+                        xytext=(0, 10), textcoords='offset points')
         plt.show()
     elif type_of_data == 'job satisfaction':
         working_dict = analysis.job_satisfaction_by_gender()
         df = pd.DataFrame(working_dict)
-        ax = df.plot(kind="bar", rot=0, title="Job Satisfaction By Gender", colormap='tab20')
+        ax = df.plot(kind="bar",
+                     rot=0,
+                     title="Job Satisfaction By Gender",
+                     colormap='tab20')
         for p in ax.patches:
-            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+            ax.annotate(p.get_height(), (p.get_x() + p.get_width() / 2.,
+                        p.get_height()), ha='center', va='center',
+                        xytext=(0, 10), textcoords='offset points')
         plt.show()
